44345ffb4bec8572023e73d12c16e93f326b8b59
Mao <maoacr@gmail.com>
2023-11-28 11:30:56 -0500

    base files running

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE =
+  "Blog de Desarrollo web, tal vez un poco de diseño y risas ! | Escrito por @maoacr";
+export const SITE_DESCRIPTION =
+  "Este es un sitio web donde escribo cuando necesito desahogarme un poco o tal vez enseñar algo que acabo de aprender y me hace muy feliz si esta información te sirve, te ayuda, te motiva o lo que sea que pase.";
