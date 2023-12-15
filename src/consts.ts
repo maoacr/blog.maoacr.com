935200217f062770ee00c47d00f07f98411780a6
Mao <maoacr@gmail.com>
2023-12-15 14:53:45 -0500

    Theme toogle suspended

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "@maoacr | Historias y risas !";
+export const SITE_TITLE = "Historias y risas !";
 export const SITE_DESCRIPTION =
   "Este es un sitio web donde escribo cuando necesito desahogarme un poco o tal vez enseñar algo que acabo de aprender y me hace muy feliz si esta información te sirve, te ayuda, te motiva o lo que sea que pase.";
