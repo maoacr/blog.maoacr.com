998d491e0e48cd8576913bbbe14d67e51b6f695b
Mao <maoacr@gmail.com>
2024-02-11 21:57:47 -0500

    email register ready

--- public/insertNewSubscriber.js
+++ public/insertNewSubscriber.js
@@ -6,12 +6,13 @@ document
 
 async function insertNewSubscriber(e) {
   e.preventDefault();
-  console.log("Me hicieron click");
   let subscriberMail = document.getElementById("inputEmail");
-  const { data, error } = await supabase
-    .from("users")
-    .insert([{ email: subscriberMail.value }])
-    .select();
-  subscriberMail.value = "";
-  console.log(data, error);
+
+  if (!subscriberMail) {
+    const { data, error } = await supabase
+      .from("users")
+      .insert([{ email: subscriberMail.value }])
+      .select();
+    subscriberMail.value = "";
+  }
 }
